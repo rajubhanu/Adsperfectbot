efe6ef775dc4f96d66b020c0f06849a5c89a5d22
rajubhanu <33171357+rajubhanu@users.noreply.github.com>
2025-07-02 15:54:17 +0530

    Update ads_bot.py

--- ads_bot.py
+++ ads_bot.py
@@ -1,5 +1,5 @@
 import logging
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 from telegram import Update
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from html import escape
